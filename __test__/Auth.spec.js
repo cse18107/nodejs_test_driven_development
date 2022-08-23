6532d144f6ec2960f987fcd9bf7ec2258af8ebc3
cse18107 <cse18107@cemk.ac.in>
2022-08-23 09:22:08 +0530

    working on auth test cases and refactoring the code

--- __test__/Auth.spec.js
+++ __test__/Auth.spec.js
@@ -12,8 +12,9 @@ beforeEach(async () => {
   await User.destroy({ truncate: true });
 });
 
-const addUser = async () => {
-  const user = { username: 'user1', email: 'user1@gmail.com', password: 'P4ssword', inActive: false };
+const activeUser = { username: 'user1', email: 'user1@gmail.com', password: 'P4ssword', inActive: false };
+
+const addUser = async (user = { ...activeUser }) => {
   const hash = await bcrypt.hash(user.password, 10);
   user.password = hash;
   return await User.create(user);
@@ -35,5 +36,44 @@ describe('Authentication', () => {
     expect(response.body.id).toBe(user.id);
     expect(response.body.username).toBe(user.username);
     expect(Object.keys(response.body)).toEqual(['id', 'username']);
+  });
+  it('returns 401 when user does not exist', async () => {
+    const response = await postAuthentication({ email: 'user1@gmail.com', password: 'P4ssword' });
+    expect(response.status).toBe(401);
+  });
+  it('returns proper error body when authentication fails', async () => {
+    const nowInMillis = new Date().getTime();
+    const response = await postAuthentication({ email: 'user1@gmail.com', password: 'P4ssword' });
+    const error = response.body;
+    expect(error.path).toBe('/api/1.0/auth');
+    expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+  });
+  it('returns 401 when password is wrong', async () => {
+    await addUser();
+    const response = await postAuthentication({ email: 'user1@gmail.com', password: 'password' });
+    expect(response.status).toBe(401);
+  });
+  it('returns 403 when logging in with an inactive account', async () => {
+    await addUser({ ...activeUser, inActive: true });
+    const response = await postAuthentication({ email: 'user1@gmail.com', password: 'P4ssword' });
+    expect(response.status).toBe(403);
+  });
+  it('returns proper error body when inActive authentication fails', async () => {
+    await addUser({ ...activeUser, inActive: true });
+    const nowInMillis = new Date().getTime();
+    const response = await postAuthentication({ email: 'user1@gmail.com', password: 'P4ssword' });
+    const error = response.body;
+    expect(error.path).toBe('/api/1.0/auth');
+    expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+  });
+  it('returns 401 when e-mail is not valid', async () => {
+    const response = await postAuthentication({ password: 'P4ssword' });
+    expect(response.status).toBe(401);
+  });
+  it('returns 401 when password is not valid', async () => {
+    const response = await postAuthentication({ email: 'user1@gmail.com' });
+    expect(response.status).toBe(401);
   });
 });
--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const UserService = require('../user/UserService');
+const AuthenticationException = require('./AuthenticationException');
+const ForbiddenException = require('./ForbiddenException');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 
-router.post('/auth',async (req, res) => {
-  const { email } = req.body;
+router.post('/auth', check('email').isEmail(), async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new AuthenticationException());
+  }
+  const { email, password } = req.body;
   const user = await UserService.findByEmail(email);
+  if (!user) {
+    return next(new AuthenticationException());
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return next(new AuthenticationException());
+  }
+  if (user.inActive) {
+    return next(new ForbiddenException());
+  }
   res.send({
     id: user.id,
     username: user.username,
